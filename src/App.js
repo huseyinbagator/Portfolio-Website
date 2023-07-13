143a8c8e5ced9627ae7552b04a32530fe1c712a8
Hüseyin Bagator <bagatorhuseyin0@gmail.com>
2023-07-14 00:14:14 +0300

    Page-routers

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-
+import React  from 'react';
 import './App.css';
-
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+import Home from './Pages/Home';
+import About from './Pages/About';
+import Techstack from './Pages/Techstack';
+import Projects from './Pages/Projects';
+import Contact from './Pages/Contact';
 function App() {
   return (
-   <>
-   
-   </>
+    <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/about' element={<About/>} />
+          <Route path='/tech-stack' element={<Techstack/>} />
+          <Route path='/projects' element={<Projects/>} />
+          <Route path='/contact' element={<Contact/>} />
+          <Route path='*' element={<Home/>} />
+        </Routes>
+      <div className='App'>
+       
+  
+        </div>
+    </BrowserRouter>
+    
   );
 }
 
