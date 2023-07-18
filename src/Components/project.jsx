2b2238298bc3cc821609753722f0097381e86a23
Hüseyin Bagator <bagatorhuseyin0@gmail.com>
2023-07-18 17:29:57 +0300

    projects-section-css

--- src/Components/project.jsx
+++ src/Components/project.jsx
@@ -1,26 +1,30 @@
 import React from 'react';
 import portfolio from '../images/portfolio.png';
+import './project.css';
 
 function project() {
 
   return (
+
     <div className='project-container'>
-        <h1>Projects</h1>
-        <h2>Things I’ve built so far</h2>
+        <h1 className='container-title'>Projects</h1>
+        <h2 className='container-description'>Things I’ve built so far</h2>
         <div className='card'>
             <div className='image'>
                 <img src={portfolio} alt="portfolio" />
             </div>
-            <div >
-                <h1 className='title'>Portfolio Website</h1>
-                <p className='description'>Hello! This is my developer portfolio website that I built using React. This site is designed to showcase my work and skills.</p>
-            </div>
-            <div className='project-links'>
+            <div className='project-description' >
+                <h1 className='title-portfolio'>Portfolio Website</h1>
+                <p className='description-portfolio'>Hello! This is my developer portfolio website that I built using React. This site is designed to showcase my work and skills.</p>
+                <p className='stack'>Tech stack : React, Css</p>
+                 <div className='project-links'>
                 <a href='https://github.com/huseyinbagator/Portfolio-Website'  alt="Github"  rel="noreferrer" target='_blank'>View Code</a>
                 
             </div>
+            </div>
+           
         </div>
-    </div>
+    </div> 
   )
 }
 
