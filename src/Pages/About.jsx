d358048e37506a91a83166c70b566ec13eb50a66
Hüseyin Bagator <bagatorhuseyin0@gmail.com>
2023-07-20 16:09:07 +0300

    about-page-1

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import React from "react";
+import './About.css';
 
 function About() {
   return (
-    <div></div>
-  )
+    <div className="about-container">
+      <div className="me">
+        <h1 className="h1-title">About Me</h1>
+        <p>
+          Hello, I am Hüseyin Bağator. I am a computer programming graduate
+          living in Kocaeli, Turkey, and working as a frontend developer. I am
+          passionate about keeping up with the rapidly changing landscape of the
+          technology world and staying up-to-date. After graduating from Karaman
+          Mehmet Bey University, I pursued various training opportunities to
+          enhance my skills in frontend development. My innovative mindset keeps
+          me constantly open to exploring new technologies and trends. My desire
+          for continuous self-improvement, collaborative attitude, and success
+          in teamwork allow me to work harmoniously with project teams. This
+          enables me to ensure that projects are completed on time and with
+          success.
+        </p>
+      </div>
+      <div className="work">
+        <div>
+          <h1 className="h1-title">Work Experience</h1>
+          <div>
+            <h2 className="title-about">Junior Frontend Developer</h2>
+            <p>Full Time Intern</p>
+          </div>
+          <div>
+            <p>Ağaçkakan Study Cafe</p>
+            <p>Turkey,Eskisehir(Remote)</p>
+            <p>01.04.2023</p>
+          </div>  
+          
+        </div>
+      </div>
+      <div className="edu">
+        <h1 className="h1-title">Education</h1>
+          <div>
+            <h2 className="title-about">Karaman Mehmet Bey University</h2>
+            <div>
+              <p className="">Computer Programming</p>
+              <p className="year">2023</p>
+            </div>
+          </div>
+          <div>
+            <h2 className="title-about">React JS : Applied React JS -Redux Tutorial </h2>
+            <div>
+              <p>Can Boz</p>
+              <p className="year">2023</p>
+            </div>
+          </div>
+          
+      </div>
+    </div>
+  );
 }
 
-export default About
+export default About;
