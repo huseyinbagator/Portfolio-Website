6427f5ea18ab3a933fd25fe6321c1a4ec5eb3f4d
Hüseyin Bagator <bagatorhuseyin0@gmail.com>
2023-07-20 10:25:33 +0300

    footer-css-update

--- src/Components/footer.jsx
+++ src/Components/footer.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import footergithub from "../images/footergithub.svg";
 import linkedinfooter from "../images/linkedin-footer.svg";
 import { Link } from "react-router-dom";
+import './footer.css'
 
 function footer() {
   return (
-    <div>
-      <div>
-        <p>bagatorhuseyin0@gmail.com</p>
-        <div>
+    <div className="footer-container">
+      <div className="person">
+        <p className="communication">0551 947 26 27</p>
+        <p className="communication">bagatorhuseyin0@gmail.com</p>
+       
+       
+        <div className="footer-social">
           <a
             target="_blank"
             href="https://github.com/huseyinbagator"
@@ -28,23 +32,27 @@ function footer() {
         </div>
       </div>
       <div className="line"></div>
-
-      <div className="footer-nav">
-        <Link className="nav-link" to="/">
-          Home
-        </Link>
-        <Link className="nav-link" to="/about">
-          About
-        </Link>
-        <Link className="nav-link" to="/tech-stack">
-          Tech Stack
-        </Link>
-        <Link className="nav-link" to="/projects">
-          Projects
-        </Link>
-        <Link className="nav-link" to="/contact">
-          Contact
-        </Link>
+      <div className="footer-bottom">
+      <div className="footer-router">
+          <Link className="nav-link" to="/">
+            Home
+          </Link>
+          <Link className="nav-link" to="/about">
+            About
+          </Link>
+          <Link className="nav-link" to="/tech-stack">
+            Tech Stack
+          </Link>
+          <Link className="nav-link" to="/projects">
+            Projects
+          </Link>
+          <Link className="nav-link" to="/contact">
+            Contact
+          </Link>
+      </div>
+      <div className="built">
+        <p>Built by Hüseyin Bağator 2023</p>  
+      </div>  
       </div>
     </div>
   );
