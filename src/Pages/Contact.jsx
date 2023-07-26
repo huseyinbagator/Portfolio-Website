c0251bae5669dd80ca57858eea5778e867e1ac46
Hüseyin Bagator <bagatorhuseyin0@gmail.com>
2023-07-26 13:01:25 +0300

    h1-update

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -4,7 +4,7 @@ import './Contact.css'
 function Contact() {
   return (
    <div className='contact-container'>
-    <h1 className='ct-title'>For any questions please mail us:</h1>
+    <h1 className='ct-title'>For any questions please mail me:</h1>
     <h2 className='ct-mail' >bagatorhuseyin0@gmail.com</h2>
   </div>
   )
